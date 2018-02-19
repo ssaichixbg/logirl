41f72b8ac678b2a05a44cae624e13a6020753435
Simon <zhangy405@gmail.com>
2018-02-19 00:15:32 -0800

    update js minify

--- static/src/gulpfile.js
+++ static/src/gulpfile.js
@@ -1,13 +1,46 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var minify = require('gulp-minify');
+var cleanCss = require('gulp-clean-css');
+var shell = require('gulp-shell');
 
 gulp.task('styles', function() {
     gulp.src('scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCss())
         .pipe(gulp.dest('../css/'));
 });
 
+gulp.task('copy-js', function () {
+    gulp.src('js/**/*.js')
+        .pipe(gulp.dest('../js/'));
+});
+
+gulp.task('copy-minify-js', function () {
+    gulp.src('js/**/*.js')
+        .pipe(minify({
+            ext:{
+                min:'.js'
+            },
+            noSource: true
+        }))
+        .pipe(gulp.dest('../js/'));
+});
+
+
+gulp.task('collectstatic', shell.task([
+    'python ../../manage.py collectstatic --noinput'
+]));
+
 gulp.task('default',function() {
     gulp.run('styles');
+    gulp.run('copy-js');
     gulp.watch('scss/**/*.scss',['styles']);
+    gulp.watch('js/**/*.js',['copy-js']);
+});
+
+gulp.task('prod', function () {
+    gulp.run('styles');
+    gulp.run('copy-minify-js');
+    gulp.run('collectstatic');
 });
